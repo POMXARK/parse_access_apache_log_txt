2a8f2dcd8b19bf3e1bc02ee30d642d521ecbe3ca
POMXARK <1997pom@gmail.com>
2022-04-01 19:15:08 +1000

    Create class

--- main.py
+++ main.py
@@ -2,59 +2,72 @@ import json
 from datetime import datetime
 import re
 from file_read_backwards import FileReadBackwards
-import threading
-from rapidfuzz import  fuzz
-
-lines = []
+from threading import Thread
+from rapidfuzz import fuzz
 
 
-def parse_access_log(path_file: str):
-    parse_logs = []
-    try:
-        with FileReadBackwards(path_file, encoding="utf-8") as frb:
-            new_lines = []
-            add_new = 0
-            for idx, l in enumerate(frb):
-                if len(lines) == 0:
-                    new_lines.append(l)
-                else:
-                    try:
-                        if fuzz.ratio(l, lines[0]) == 100:
+class ParserAccessLog(Thread):
+
+    def __init__(self, path_file):
+        super().__init__()
+        self.path_file = path_file
+        self.new_lines = []
+        self.parse_logs = []
+        self.lines = []
+
+    def _read_file(self):
+        self.parse_logs = []
+        try:
+            with FileReadBackwards(self.path_file, encoding="utf-8") as frb:
+                self.new_lines = []
+                for idx, l in enumerate(frb):
+                    if len(self.lines) == 0:
+                        self.new_lines.append(l)
+                    else:
+                        try:
+                            if fuzz.ratio(l, self.lines[0]) == 100:
+                                break
+                            else:
+                                self.new_lines.append(l)
+                        except IndexError:
                             break
-                        else:
-                            new_lines.append(l)
-                    except IndexError:
-                        break
-            if len(new_lines) == 0:
-                return
-    except PermissionError or IndexError:
-        return
+                if len(self.new_lines) == 0:
+                    return
+                else:
+                    return self._parse_file()
 
-    for el in new_lines:
-        ip = re.findall(r"^\d+[.]\d+[.]\d+", el)
-        if len(ip) == 0:
-            ip = re.findall(r"^\d+[:]\d+[:]\d+", el)
+        except PermissionError or IndexError:
+            return
+
+    def _parse_file(self):
+        for el in self.new_lines:
+            ip = re.findall(r"^\d+[.]\d+[.]\d+", el)
             if len(ip) == 0:
-                ip = re.findall(r"::\d+", el)
-        date = str(datetime.strptime(re.findall(r"\d+[/]\D+[/]\d+[:]\d+[:]\d+[:]\d+", el)[-1], '%d/%b/%Y:%H:%M:%S'))
-        data = re.split(r"] ", el)[-1]
-        if len(ip) == 1:
-            data = data.replace('\"', '')
-            parse_logs.append({"ip": ip[-1], "date": date, "data": data})
-    # else:
-    for idx, el in enumerate(new_lines):
-        lines.insert(idx, el)
+                ip = re.findall(r"^\d+[:]\d+[:]\d+", el)
+                if len(ip) == 0:
+                    ip = re.findall(r"::\d+", el)
+            date = str(datetime.strptime(re.findall(r"\d+[/]\D+[/]\d+[:]\d+[:]\d+[:]\d+", el)[-1], '%d/%b/%Y:%H:%M:%S'))
+            data = re.split(r"] ", el)[-1]
+            if len(ip) == 1:
+                data = data.replace('\"', '')
+                self.parse_logs.append({"ip": ip[-1], "date": date, "data": data})
+        for idx, el in enumerate(self.new_lines):
+            self.lines.insert(idx, el)
 
-    return json.dumps({"data": parse_logs})
+        return json.dumps({"data": self.parse_logs})
+
+    def _update_json(self):
+        while True:
+            json = self._read_file()
+            if json is not None:
+                print(json)
+
+    def run(self):
+        self._update_json()
 
 
-def update_json():
-    while True:
-        json = parse_access_log(r"C:\xampp\apache\logs\access.log")
-        if json is not None:
-            print(json)
+if __name__ == '__main__':
+    thread1 = ParserAccessLog(r"access.log")
+    thread1.start()
 
-
-
-thr1 = threading.Thread(target=update_json, args=()).start()
 print('основной поток')
